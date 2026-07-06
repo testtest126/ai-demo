0ee93bf8f50c59d1ccb8ee98f64790a2bffa4558
Yakiv Kovalskyi <ykovalsky.up@gmail.com>
2026-07-06 15:56:07 +0200

    Initial Commit

---
+++ Core AI Language Model TestTests/Core_AI_Language_Model_TestTests.swift
@@ -0,0 +1,18 @@
+//
+//  Core_AI_Language_Model_TestTests.swift
+//  Core AI Language Model TestTests
+//
+//  Created by Y K on 06.07.26.
+//
+
+import Testing
+
+struct Core_AI_Language_Model_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ Core AI Language Model TestUITests/Core_AI_Language_Model_TestUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  Core_AI_Language_Model_TestUITestsLaunchTests.swift
+//  Core AI Language Model TestUITests
+//
+//  Created by Y K on 06.07.26.
+//
+
+import XCTest
+
+final class Core_AI_Language_Model_TestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
